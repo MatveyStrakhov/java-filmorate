50ac967dc91e10e3c8a3072c3c3f73ca5043f653
MatveyStrakhov <matveystrakhov@yandex.ru>
2023-07-16 16:42:33 +0300

    style: codestyle 3

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -7,7 +7,6 @@ import org.springframework.lang.Nullable;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 import java.time.LocalDate;
 
 @Data
