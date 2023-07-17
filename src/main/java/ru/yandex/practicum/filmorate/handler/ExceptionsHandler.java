3989fbbafa9f9fe3d6b915fe505e9ddddda88baf
MatveyStrakhov <matveystrakhov@yandex.ru>
2023-07-18 00:39:57 +0300

    fix: added error messages

--- src/main/java/ru/yandex/practicum/filmorate/handler/ExceptionsHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/handler/ExceptionsHandler.java
@@ -2,24 +2,23 @@ package ru.yandex.practicum.filmorate.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.yandex.practicum.filmorate.exception.IncorrectIdException;
 
 @Slf4j
 @ControllerAdvice
 public class ExceptionsHandler {
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    void handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        log.warn(e.getMessage());
+    ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.warn("Validation failed:" + e.getMessage());
+        return new ResponseEntity<>("Validation failed: " + e.getFieldError().getDefaultMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = IncorrectIdException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-   void handleIncorrectIdException(IncorrectIdException e) {
-        log.warn(e.getMessage());
+    ResponseEntity<Object> handleIncorrectIdException(IncorrectIdException e) {
+        return new ResponseEntity<>("ID not found", HttpStatus.NOT_FOUND);
     }
 }
