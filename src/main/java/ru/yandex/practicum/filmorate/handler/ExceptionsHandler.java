82363ea7c9106f8ec0393691f8d83faaf2a7237c
MatveyStrakhov <matveystrakhov@yandex.ru>
2023-07-18 14:13:10 +0300

    fix: added new error jsons

--- src/main/java/ru/yandex/practicum/filmorate/handler/ExceptionsHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/handler/ExceptionsHandler.java
@@ -1,5 +1,8 @@
 package ru.yandex.practicum.filmorate.handler;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.Builder;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,18 +10,37 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.yandex.practicum.filmorate.exception.IncorrectIdException;
+import java.time.LocalDateTime;
 
 @Slf4j
 @ControllerAdvice
 public class ExceptionsHandler {
+    @Data
+    @Builder
+    private static class ErrorJson {
+        int status;
+        String error;
+        LocalDateTime timestamp;
+    }
+
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         log.warn("Validation failed:" + e.getMessage());
-        return new ResponseEntity<>("Validation failed: " + e.getFieldError().getDefaultMessage(), HttpStatus.BAD_REQUEST);
+        ErrorJson error = ErrorJson.builder()
+                .error("Validation failed: " + e.getFieldError().getDefaultMessage())
+                .timestamp(LocalDateTime.now())
+                .status(400)
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = IncorrectIdException.class)
-    ResponseEntity<Object> handleIncorrectIdException(IncorrectIdException e) {
-        return new ResponseEntity<>("ID not found", HttpStatus.NOT_FOUND);
+    ResponseEntity<Object> handleIncorrectIdException(IncorrectIdException e) throws JsonProcessingException {
+        ErrorJson error = ErrorJson.builder()
+                .error("ID not found")
+                .timestamp(LocalDateTime.now())
+                .status(404)
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
