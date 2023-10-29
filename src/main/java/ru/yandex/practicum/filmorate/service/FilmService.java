ff36fc0b8c626eedb9150163ceb27e90272ccae6
XynA <YaXymma@yandex.ru>
2023-10-29 21:11:51 +0300

    метод getPopularFilms заменен на перегрузку findPopularFilms добавлен метод удаления

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -26,11 +26,6 @@ public class FilmService {
         filmStorage.unlikeFilm(filmId, userId);
     }
 
-
-    public List<Film> getPopularFilms(int count) {
-        return filmStorage.getPopularFilms(count);
-    }
-
     public List<Film> getFilmsByDirector(int directorId, String sortBy) {
         return filmStorage.getFilmsByDirector(directorId, sortBy);
     }
@@ -59,4 +54,60 @@ public class FilmService {
     public List<Film> searchFilms(String query, String by) {
         return filmStorage.searchFilms(query, by);
     }
+
+    //возвращает список первых фильмов по количеству лайков.
+    public List<Film> findPopularFilms(Integer count) {
+        if (count <= 0) {
+            throw new IdNotFoundException("count");
+        }
+
+        if (filmStorage.findPopularFilms(count) != null) {
+            return filmStorage.findPopularFilms(count);
+        } else {
+            return null;
+        }
+    }
+
+    // поиск популярных фильмов по году
+    public List<Film> findPopularFilms(Integer count, Integer year) {
+        if (count <= 0) {
+            throw new IdNotFoundException("count");
+        }
+
+        if (filmStorage.findPopularFilms(count, year) != null) {
+            return filmStorage.findPopularFilms(count, year);
+        } else {
+            return null;
+        }
+    }
+
+    // поиск популярных фильмов по жанру
+    public List<Film> findPopularFilms(Integer count, Long genreId) {
+        if (count <= 0) {
+            throw new IdNotFoundException("count");
+
+        }
+        if (filmStorage.findPopularFilms(count, genreId) != null) {
+            return filmStorage.findPopularFilms(count, genreId);
+        } else {
+            return null;
+        }
+    }
+
+    // поиск популярных фильмов по году и жанру
+    public List<Film> findPopularFilms(Integer count, Long genreId, Integer year) {
+        if (count <= 0) {
+            throw new IdNotFoundException("count");
+        }
+        if (filmStorage.findPopularFilms(count, genreId, year) != null) {
+            return filmStorage.findPopularFilms(count, genreId, year);
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteFilm(int filmId) {
+        filmStorage.deleteFilm(filmId);
+    }
+
 }
