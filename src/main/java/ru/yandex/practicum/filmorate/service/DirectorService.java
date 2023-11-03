8638183d8b51918eb85137f40c8224d5d47023f6
Taratorin <unknownmanty@yandex.ru>
2023-11-03 16:43:58 +0300

    создать интерфейс для методов класса DirectorService;

--- src/main/java/ru/yandex/practicum/filmorate/service/DirectorService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/DirectorService.java
@@ -10,29 +10,35 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 
-public class DirectorService {
+public class DirectorService implements IDirectorService {
     private final DirectorDao directorDao;
 
+    @Override
     public Director getDirectorById(int directorId) {
         return directorDao.getDirectorById(directorId);
     }
 
+    @Override
     public Director updateDirector(Director director) {
         return directorDao.updateDirector(director);
     }
 
+    @Override
     public List<Director> getAllDirectors() {
         return directorDao.getAllDirectors();
     }
 
+    @Override
     public Director createDirector(Director director) {
         return directorDao.createDirector(director);
     }
 
+    @Override
     public void deleteDirector(int directorId) {
         directorDao.deleteDirector(directorId);
     }
 
+    @Override
     public boolean isValidDirector(int directorId) {
         return directorDao.isValidDirector(directorId);
     }
